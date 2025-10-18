ad27085427f468da32bd15d19ef47d5efc83eff1
Brayan <02hxbrayan@gmail.com>
2025-10-18 11:58:07 -0500

    Fase 1: Estructura de Componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,Footer,About,Skills],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
