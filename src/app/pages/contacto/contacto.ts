b335b2286c12670fb70514a277dcba0332d48b85
Brayan <02hxbrayan@gmail.com>
2025-10-27 14:10:52 -0500

    Fase 4: Eventos y Binding completada

--- src/app/pages/contacto/contacto.ts
+++ src/app/pages/contacto/contacto.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contacto.html',
   styleUrl: './contacto.css'
 })
+
 export class Contacto {
 
   public correoCopiado: boolean = false; 
+
+  public urlGithub: string = "https://github.com/BrayanPahuara";
+
+  public asunto: string = "";
 
   copiarCorreo() {
     const correo = "02hxbrayan@gmail.com";
